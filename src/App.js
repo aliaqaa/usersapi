b202796803da5a0ab6406b1e49622674ed92524f
aliaqaa <ali.tousi77@gmail.com>
2024-06-27 15:24:53 +0330

    first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+const url = 'https://api.github.com/users';
+
+function App() {
+  const [users, setUsers] = useState([]);  // Correctly declare state using array destructuring
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+
+    console.log(users);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {users.map(user => (
+          <li key={user.id}><img src='{user.avatar-url}'></img> {user.login}</li>  // Properly map users to list items with unique key
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
